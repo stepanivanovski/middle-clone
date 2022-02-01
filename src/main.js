77fadc57a438c60715b8e7b38a28bb14ac94fa70
Stepan <stepanivanovski@gmail.com>
2022-02-01 11:01:35 +0600

    add Register Page

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
+// import { userSetter } from 'core-js/fn/symbol'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+createApp(App)
+  .use(router)
+  .use(store)  
+  .mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,22 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+// import Vue from 'vue'
+import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../views/Home.vue'
-
-Vue.use(VueRouter)
+import Register from '@/views/Register'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/register',
+    name: 'register',
+    component: Register
   }
 ]
 
-const router = new VueRouter({
-  routes
+export default createRouter({
+  routes,
+  history: createWebHistory()
 })
-
-export default router
